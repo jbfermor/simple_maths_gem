f866e32add747225e4a98fcee434cf0650d6d12e
jbfermor <jbfermor@hotmail.com>
2022-10-06 19:22:43 +0200

    init

---
+++ lib/simple_maths.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simple_maths/version"
+
+module SimpleMaths
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/simple_maths/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SimpleMaths
+  VERSION = "0.1.0"
+end
