40b559e0c0e83e5b78312f8f865e222986ed499f
jbfermor <jbfermor@hotmail.com>
2022-10-06 19:22:59 +0200

    methods

--- lib/simple_maths.rb
+++ lib/simple_maths.rb
@@ -4,5 +4,20 @@ require_relative "simple_maths/version"
 
 module SimpleMaths
   class Error < StandardError; end
-  # Your code goes here...
+  
+  def self.suma(x, y)
+    x + y
+  end
+
+  def self.resta(x, y)
+    x - y
+  end
+
+  def self.producto(x,y)
+    x * y
+  end
+
+  def self.division(x,y)
+    x / y
+  end
 end
--- spec/simple_maths_spec.rb
+++ spec/simple_maths_spec.rb
@@ -4,8 +4,32 @@ RSpec.describe SimpleMaths do
   it "has a version number" do
     expect(SimpleMaths::VERSION).not_to be nil
   end
+  
+  describe "operaciones" do
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    it "suma dos numeros" do
+      x = 2
+      y = 3
+      expect(SimpleMaths.suma(x,y)).to eq(5)
+    end
+
+    it "resta dos numeros" do
+      x = 2
+      y = 3
+      expect(SimpleMaths.resta(x,y)).to eq(-1)
+    end
+
+    it "multiplica dos numeros" do
+      x = 2
+      y = 3
+      expect(SimpleMaths.producto(x,y)).to eq(6)
+    end
+
+    it "divide dos numeros" do
+      x = 4
+      y = 2
+      expect(SimpleMaths.division(x,y)).to eq(2)
+    end
+
   end
 end
