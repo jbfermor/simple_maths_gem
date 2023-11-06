22d72289a9a0ce6deb5295bea168889cbbd31f38
jbfermor <jbfermor@hotmail.com>
2023-11-06 16:36:04 +0100

    restore

--- lib/simple_maths.rb
+++ lib/simple_maths.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "simple_maths/version"
-require_relative "simple_maths/operations/operations"
 
-class SimpleMaths
-
-  def self.operation(operation, n1, n2)
-    tested = SimpleMaths::Operations::Operations.new(operation, n1, n2)
-    tested.operation
-
-  end
+module SimpleMaths
+  class Error < StandardError; end
   
   def self.suma(x, y)
     x + y
@@ -26,5 +20,4 @@ class SimpleMaths
   def self.division(x,y)
     x / y
   end
-
 end
